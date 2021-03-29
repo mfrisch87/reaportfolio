9d9ad251ea0a8f4ad8f53b7d179cdd89b9883734
mfrisch87 <mikey.frisch@gmail.com>
2021-03-29 15:16:53 -0700

    files didn't push for some reason

---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Michael Frisch",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"michael-frisch-21a438144",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Home-Chef trying to turn myself into the best Full-Stack Developer I can be",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/michael-frisch-21a438144/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mfrisch87",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.instagram.com/trondog/",
+          "className":"instagramr"
+        }
+      ],
+    "aboutme":"I am fresh out of the UW Coding BootCamp and looking to hone my skills in a professional setting. I am constantly motivated to learn and improve as much as possible.",
+    "address":"Renton, WA",
+    "website":"https://github.com/mfrisch87",
+    "education":[
+      {
+        "UniversityName":"Univeristy of Washington",
+        "specialization":"Coding Boot Camp - Web Development",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2021",
+        "Achievements":"Learning Full Stack Web Development"
+      },
+      {
+        "UniversityName":"Highline College",
+        "specialization":"Accounting",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2015",
+        "Achievements":"Associates in Accounting"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"DCGONE",
+        "specialization":"Senior Application Support Specialist",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Apple Retail Store - Bellevue, WA",
+        "specialization":"Inventory Control Specialist",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2008",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+  
+  }
+  
+  export default resumeData
